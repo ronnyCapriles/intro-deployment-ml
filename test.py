ecc01fb6ba79deb9ba2f0b7bc526baa12d49defe
ronnyCapriles <ronnycapriles47@gmail.com>
2023-12-11 09:47:07 -0400

    Adding testing

---
+++ test.py
@@ -0,0 +1,40 @@
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+
+
+def test_null_prediction():
+
+    dict_json = {
+        "opening_gross": 0,
+        "screens": 0,
+        "production_budget": 0,
+        "title_year": 0,
+        "aspect_ratio": 0,
+        "duration": 0,
+        "cast_total_facebook_likes": 0,
+        "budget": 0,
+        "imdb_score": 0
+        }
+
+    response = client.post('/v1/prediction', json = dict_json)
+    assert response.status_code == 200
+    assert response.json()['worldwide_gross'] == 0
+
+def test_random_prediction():
+    dict_json = {
+        "opening_gross": 8338681,
+        "screens": 2271,
+        "production_budget": 13000000,
+        "title_year": 1999,
+        "aspect_ratio": 1.85,
+        "duration": 97,
+        "cast_total_facebook_likes": 37907,
+        "budget": 16000000,
+        "imdb_score": 7.2
+        }
+    response = client.post('/v1/prediction', json = dict_json)
+    assert response.status_code == 200
+    assert response.json()['worldwide_gross'] != 0
